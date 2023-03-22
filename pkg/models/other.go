c673b44e6d28ecbdbd03113b77d852a9c33c9c44
School_Bozhinov <marogo142005@gmail.com>
2023-03-22 12:31:06 +0200

    [ADD] Vote Request Body

---
+++ pkg/models/other.go
@@ -0,0 +1,10 @@
+package models
+
+type VoteRequest struct {
+	Name        string `json:"name"`
+	Email       string `json:"email"`
+	SoftwareID  uint   `json:"software_id"`
+	NetworksID  uint   `json:"networks_id"`
+	EmbeddedID  uint   `json:"embedded_id"`
+	BattleBotID uint   `json:"battlebot_id"`
+}
