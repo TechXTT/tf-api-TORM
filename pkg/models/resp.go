abea9e72c5709781cbd4decd667d9d210fcddb89
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-04-13 21:05:14 +0300

    [ADD] Video to category get

--- pkg/models/resp.go
+++ pkg/models/resp.go
@@ -54,6 +54,7 @@ type GetProjectByCategory struct {
 	ID        uint   `json:"id"`
 	Name      string `json:"name"`
 	Thumbnail string `json:"thumbnail"`
+	Video     string `json:"video"`
 }
 type PostVote struct {
 	Msg string `json:"msg"`
